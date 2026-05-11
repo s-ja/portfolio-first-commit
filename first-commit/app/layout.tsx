237a5cb016248733c349315e04afa541be3e2d3c
Vercel <vercel[bot]@users.noreply.github.com>
2026-05-11 08:46:55 +0000

    Add Vercel Web Analytics integration
    This is an automated pull request generated by Vercel to help install [Web Analytics](https://vercel.com/docs/analytics) in your application.
    
    Co-authored-by: Vercel <vercel[bot]@users.noreply.github.com>

--- first-commit/app/layout.tsx
+++ first-commit/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,7 @@ export default function RootLayout({
         >
           {children}
           <SpeedInsights />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
